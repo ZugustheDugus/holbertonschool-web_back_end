3a8038c7c23a143f347a00445069bcd7ca709b5d
ZugustheDugus <h1cooper2019@protonmail.com>
2022-05-06 11:19:27 -0500

    LF not CRLF

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
-export default function appendToEachArrayValue (array, appendString) {
-  const newarray = []
-  for (const idx of array) {
-    newarray.push(appendString + idx)
+export default function appendToEachArrayValue(array, appendString) {
+    const newarray = [];
+    for (const idx of array) {
+      newarray.push(appendString + idx);
+    }
+  
+    return newarray;
   }
-
-  return newarray
-}
