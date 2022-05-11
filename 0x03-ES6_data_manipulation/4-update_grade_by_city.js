76f5d95357d3d3c63c9d79696f76dfd1cc75322e
ZugustheDugus <h1cooper2019@protonmail.com>
2022-05-11 17:05:09 -0500

    asdf

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,8 +2,8 @@ export default function updateStudentGradeByCity(studentList, city, newGrades) {
   return studentList
     .filter((student) => student.location === city)
     .map((student) => {
-      const stuGrade = newGrades.filter((grade) => grade.studentId === student.id);
-      const grade = stuGrade.length ? stuGrade[0].grade : 'N/A';
+      const stGrade = newGrades.filter((grade) => grade.studentId === student.id);
+      const grade = stGrade.length ? stGrade[0].grade : 'N/A';
       return { ...student, grade };
     });
 }
