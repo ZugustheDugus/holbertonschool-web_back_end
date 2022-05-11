ec6786f8aad0441cf1a58e48ba9ee40e2761f572
ZugustheDugus <h1cooper2019@protonmail.com>
2022-05-11 15:28:45 -0500

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const nMap = new Map();
+  nMap.set('Apples', 10);
+  nMap.set('Tomatoes', 10);
+  nMap.set('Pasta', 1);
+  nMap.set('Rice', 1);
+  nMap.set('Banana', 5);
+  return nMap;
+}
