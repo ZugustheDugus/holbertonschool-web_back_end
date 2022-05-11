c3d2cd67dd26ab2d0a478068b8068d394963a5b8
ZugustheDugus <h1cooper2019@protonmail.com>
2022-05-11 16:43:36 -0500

    changed entire file into a function, rather than setting const and returning with .map

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,7 @@
-const students = [
-  { id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Fransisco' },
-];
-
-export default function getListStudents(student) {
-  return [student.id, student.firstName, student.location].join(' ');
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillame', location: 'San Fransisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Fransisco' },
+  ];
 }
-
-students.map(getListStudents);
