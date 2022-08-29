a2139bc8e330b29dcc153cac02a0c26d24dd4383
ZugustheDugus <h1cooper2019@protonmail.com>
2022-08-29 18:02:27 -0500

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Function to take an input list and return the sum of its contents"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of the input list as a float"""
+    return sum(input_list)
