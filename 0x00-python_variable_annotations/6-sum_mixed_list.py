bbd1f593a1de42df93c8996743bfbacaf4c57a8a
ZugustheDugus <h1cooper2019@protonmail.com>
2022-08-29 18:10:32 -0500

    switched order of 'int' and 'float' in the type definition of the function

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """returns a float from the mixed list of ints and floats"""
     return sum(mxd_lst)
