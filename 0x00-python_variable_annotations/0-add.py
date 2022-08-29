1c01d38a9c7dd16e008aacad980474c85daf1c8a
ZugustheDugus <h1cooper2019@protonmail.com>
2022-08-29 17:23:30 -0500

    documentation fix?

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-""" Function to add two floats and return their type """
+""" Function to add two floats and return their type
+"""
 
 
 def add(a: float, b: float) -> float:
+    """ Sums a + b and returns a result
+    """
     return (a + b)
