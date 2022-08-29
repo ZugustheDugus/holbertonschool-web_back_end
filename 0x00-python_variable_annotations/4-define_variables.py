de9950f560374175f68ca520b60e63b232221e5a
ZugustheDugus <h1cooper2019@protonmail.com>
2022-08-29 17:47:40 -0500

    task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A series of annotated variables and their definitions"""
+
+
+from xmlrpc.client import boolean
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
