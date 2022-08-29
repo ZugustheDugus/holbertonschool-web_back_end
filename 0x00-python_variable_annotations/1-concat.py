cc010d63a483d6be27b20b5f41cb2157170b3950
ZugustheDugus <h1cooper2019@protonmail.com>
2022-08-29 17:24:36 -0500

    better documentation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-""" Function to add two strings and return their type """
+""" Function to add two strings and return their type
+"""
 
 
 def concat(str1: str, str2: str) -> str:
+    """concatenates two strings and returns
+    """
     return str1 + str2
