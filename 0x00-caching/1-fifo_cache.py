6fdc5168a0651cafcab8fec330c9f6d39b3772bb
ZugustheDugus <h1cooper2019@protonmail.com>
2022-09-07 15:33:56 -0500

    task 1

---
+++ 0x00-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""First In First Out module for caching data"""
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOcache class inherits from BaseCaching"""
+
+    def __init__(self):
+        """Inits the module"""
+        self = super().__init__()
+
+    def put(self, key, item):
+        """Adds item to the cache. Deletes first item in cache if too many present"""
+        if key and item:
+            self.cache_data[key] = item
+
+        dict_list = list(self.cache_data)
+        if ((len(self.cache_data) > super().MAX_ITEMS)):
+            del self.cache_data[dict_list[0]]
+            print("DISCARD:", dict_list[0])
+
+    def get(self, key):
+        """Retrieves the key value of an item"""
+        if key and self.cache_data.get(key):
+            return self.cache_data.get(key)
+        else:
+            return None
