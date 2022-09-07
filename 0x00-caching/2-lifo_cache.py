b9d59c9ad1c5ce2979370b1bb5afab069ad8bd5b
ZugustheDugus <h1cooper2019@protonmail.com>
2022-09-07 15:46:23 -0500

    task 2

---
+++ 0x00-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Last In First Out module for caching data"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOcache class inherits from BaseCaching"""
+
+    def __init__(self):
+        """Inits the module"""
+        self = super().__init__()
+
+    def put(self, key, item):
+        """Adds item to cache. Deletes last item in cache if too many"""
+        if key and item:
+            self.cache_data[key] = item
+
+        dict_list = list(self.cache_data)
+        lencache = len(self.cache_data)
+        if (lencache > super().MAX_ITEMS):
+            del self.cache_data[dict_list[lencache - 1]]
+            print("DISCARD:", dict_list[lencache - 1])
+
+    def get(self, key):
+        """Retrieves the key value of an item"""
+        if key and self.cache_data.get(key):
+            return self.cache_data.get(key)
+        else:
+            return None
