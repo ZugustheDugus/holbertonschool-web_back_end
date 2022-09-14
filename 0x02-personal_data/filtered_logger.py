a6f26e3527f69d26635b85366150c8dd73ac9af2
ZugustheDugus <h1cooper2019@protonmail.com>
2022-09-14 17:12:54 -0500

    task 2 - pycodestyle

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -3,13 +3,13 @@
 """
 
 
-from pickle import FALSE
 import re
 from typing import List
 import logging
 
 
 PII_FIELDS = ['name', 'email', 'phone', 'ssn', 'password']
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -28,17 +28,19 @@ class RedactingFormatter(logging.Formatter):
         """Formats the message by filtering using
         filter_datum"""
         return filter_datum(self.fields, self.REDACTION,
-            super().format(record), self.SEPARATOR)
+                            super().format(record), self.SEPARATOR)
+
 
 def filter_datum(fields: List[str],
-                redaction: str,
-                message: str,
-                separator: str) -> str:
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """Returns a message with sensitive data redacted"""
     for i in fields:
         message = re.sub(f'{i}=.+?{separator}',
-                        f'{i}={redaction}{separator}', message)
+                         f'{i}={redaction}{separator}', message)
     return message
+
 
 def get_logger() -> logging.Logger:
     """Function returns logger and handles user data
