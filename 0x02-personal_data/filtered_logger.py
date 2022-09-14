2e6fac63d2d08284a66e3430e369cba9275d2990
ZugustheDugus <h1cooper2019@protonmail.com>
2022-09-14 16:33:34 -0500

    task 1

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -5,13 +5,34 @@
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Init Constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the message by filtering using
+        filter_datum"""
+        return filter_datum(self.fields, self.REDACTION,
+            super().format(record), self.SEPARATOR)
 
 def filter_datum(fields: List[str],
                 redaction: str,
                 message: str,
                 separator: str) -> str:
-    ''' Returns a message with sensitive data redacted '''
-    for x in fields:
-        message = re.sub(f'{x}=.+?{separator}',
-                        f'{x}={redaction}{separator}', message)
+    """Returns a message with sensitive data redacted"""
+    for i in fields:
+        message = re.sub(f'{i}=.+?{separator}',
+                        f'{i}={redaction}{separator}', message)
     return message
