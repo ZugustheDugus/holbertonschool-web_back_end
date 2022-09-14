99f0a96264229bb53ef5e84bf3d3a53a1420c016
ZugustheDugus <h1cooper2019@protonmail.com>
2022-09-14 17:17:02 -0500

    removed list parameter from stream_handler.setFormatter

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -50,7 +50,7 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
 
     stream_handler = logging.StreamHandler
-    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    stream_handler.setFormatter(RedactingFormatter((PII_FIELDS)))
     logger.addHandler(stream_handler)
 
     return logger
